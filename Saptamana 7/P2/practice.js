ea9d414eecef0998e4238fff47201b24e2698a8f
anamariadariescu <anamaria.dariescu@gmail.com>
2020-03-28 18:54:33 +0200

    practice

--- Saptamana 7/P2/practice.js
+++ Saptamana 7/P2/practice.js
@@ -23,3 +23,82 @@ var myFunction = function(email) {
   
   }
   myFunction("i am superman");
+
+  // 3
+  function word(value) {
+    var myArray = value.split('');
+    i=0;
+    while ( i < myArray.length) {
+      if ( myArray[i] == myArray[i].toUpperCase()) {
+          myArray[i] = myArray[i].toLowerCase();
+      } else {
+          myArray[i] = myArray[i].toUpperCase();
+      };
+      i++;
+    }
+    return myArray.join('');
+  };
+  console.log(word('abDEfGhI'));
+
+  // 4
+  function repeatWords(word, number) {
+    var result = '';
+    for( i = 1; i <= number; i++) { 
+      result = result + word;
+    }
+    console.log(result);
+  }
+  repeatWords('Wantsome',5);
+
+  // 5
+  function Palindrome(value) {
+    var checkPalindrome = value.split('');
+      if( checkPalindrome.reverse().join('') === value ) {
+          return value + " is palindrome";
+    } else {
+          return "it is not palindrome";
+    }
+  };
+
+  console.log(Palindrome('ana'));
+
+  // 6
+  var myArray = [
+    [1, 5, 9],
+    [6, 15, 8],
+    [19, 4, 10]
+  ];
+
+  function highNumber(arrayNumbers) {
+    for( i = 0; i <= arrayNumbers.length; i++) {
+      var aux = 0;
+      for( j = 0; j <= arrayNumbers[i].length; j++) {
+        if( arrayNumbers[i][j] >= aux ) {
+          aux=arrayNumbers[i][j];
+        }
+        
+      }
+        arrayNumbers[i]=aux;
+        console.log(arrayNumbers);
+    }
+  }
+
+  highNumber(myArray);
+
+  // 7
+  function myWords(word) {
+    var wordReverse = word.split('');
+    console.log(wordReverse.reverse().join(''));
+    };
+
+  myWords('bla');
+
+  // 8
+  function multiplication(a) {
+
+    var result = 1
+      for( i = 0; i <= a; i++ ) {
+        result = result * i;
+      }
+    }
+    console.log(5);
