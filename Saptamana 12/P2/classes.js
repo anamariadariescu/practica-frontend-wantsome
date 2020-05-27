d22d9c27c4fca27aa3d003d0a07807d14842c1b7
anamariadariescu <anamaria.dariescu@gmail.com>
2020-05-27 11:22:25 +0300

    practice

--- Saptamana 12/P2/classes.js
+++ Saptamana 12/P2/classes.js
@@ -68,3 +68,52 @@ console.log(elephants.walking());
 console.log(elephants.rage());
 console.log(elephants.showBehaviour());
 console.log(elephants.dailyRoutine());
+
+// Exercise 3
+//1. creati o clasa Account care sa aiba location si name la constructor
+//2. creati o clasa Transaction care sa iba sender, bank, amount si reference la constructor
+//2.1 - clasa Transaction sa aiba un getter finalSum() care sa scada din amount 10;
+//3. creati o clasa Table care sa aiba in cunstructor un array gol transactions
+//3.1 Clasa Table sa aiba o metoda addTransaction unde sa putem adauga tranzactiile pe care o sa le creem mai tarziu
+
+
+// const bankAccount = ...;
+// const transaction1 = ...;
+// const transaction2 = ...;
+// const table = ...;
+// table.addTransaction(...)
+// console.log(table)
+
+class Account {
+    constructor(name, location) {
+        this.name = name
+        this.location = location
+    }
+};
+
+class Transaction {
+    constructor(sender, bank, amount, reference) {
+        this.sender = sender
+        this.bank = bank
+        this.amount = amount
+        this.reference = reference
+    }
+    get finalSum() {
+        return this.amount - 10;
+    }
+};
+
+class Table {
+    transaction = []
+    addTransaction(...param) {
+        this.transaction.push(...param)
+    }
+};
+
+const bankAccount = new Account('Ana', 'Iasi');
+const transaction1 = new Transaction('Sandra', 'ing', 100, 'baniDeVacanta');
+const transaction2 = new Transaction('Andrei', 'ing', 50, 'pizza');
+const table = new Table();
+
+table.addTransaction(transaction1, transaction2)
+console.log(table);
