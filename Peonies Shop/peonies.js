fdf24cec82a34ff2edfd6777526029b0b6219c97
anamariadariescu <anamaria.dariescu@gmail.com>
2020-06-12 14:14:03 +0300

    peonies

--- Peonies Shop/peonies.js
+++ Peonies Shop/peonies.js
@@ -1,10 +1,12 @@
 
 var valueOfBouquet = document.getElementsByName('price');
 
-for(i=0; i<valueOfBouquet.length; i++) {
-    // if(valueOfBouquet[i].checked)
-    // console.log(valueOfBouquet[i].value)
-    valueOfBouquet[i].onclick = function() {
+for (i = 0; i < valueOfBouquet.length; i++) 
+{
+    console.log(productParentNode);
+    valueOfBouquet[i].onclick = function () {
+    console.log(productParentNode);
+
         productParentNode.innerHTML = '';
 
         var classSeason = document.createElement('div');
@@ -12,10 +14,10 @@ for(i=0; i<valueOfBouquet.length; i++) {
         // classSeason.innerHTML = 'season';
         productParentNode.appendChild(classSeason);
 
-    
+
         // alert(this.value);
 
-        switch(this.value) {
+        switch (this.value) {
             case '50 - 99':
                 for (i = 0; i < priceArrays.length; i++) {
                     if (priceArrays[i] >= 50 && priceArrays[i] <= 99) {
@@ -33,7 +35,7 @@ for(i=0; i<valueOfBouquet.length; i++) {
                 }
                 break;
             case '200 - 299':
-                for (i = 0; i <priceArrays.length; i++) {
+                for (i = 0; i < priceArrays.length; i++) {
                     if (priceArrays[i] >= 200 && priceArrays[i] <= 299) {
                         classSeason.appendChild(productNodes[i])
                     }
@@ -60,17 +62,53 @@ var productParentNode = document.querySelector('.productParent');
 var productParentSeasonNode = document.querySelectorAll('.productParent .season');
 
 var productNodes = document.querySelectorAll('.productParent .season .product');
-var myProductsPrice =  document.querySelectorAll('.productParent .season .product .product-description p');
+var myProductsPrice = document.querySelectorAll('.productParent .season .product .product-description p');
 var priceArrays = [];
 
-for (i=0; i<myProductsPrice.length; i++) {
+for (i = 0; i < myProductsPrice.length; i++) {
     priceArrays.push(parseInt(myProductsPrice[i].innerHTML.slice(0, -7)));
-
 }
 
-// console.log(priceArrays);
 
-// for (i=0; i<priceArrays.length; i++) {
+var seasonParent = document.querySelectorAll('.season');
+console.log(seasonParent);
 
-// }
+var springSeason = document.querySelectorAll('.season')[0];
+console.log(springSeason);
 
+var summerSeason = document.querySelectorAll('.season')[1];
+console.log(summerSeason);
+
+var autumnSeason = document.querySelectorAll('.season')[2];
+console.log(autumnSeason);
+
+var winterSeason = document.querySelectorAll('.season')[3];
+console.log(winterSeason);
+
+var whichSeason = document.getElementsByName('season');
+console.log(whichSeason)
+
+for (i = 0; i < whichSeason.length; i++) {
+    console.log(whichSeason[i]);
+    whichSeason[i].onclick = function () {
+        productParentNode.innerHTML = '';
+        // console.log(this.value);
+        switch(this.value) {
+            case 'Spring':
+                productParentNode.appendChild(springSeason)
+                break;
+
+            case 'Summer':
+                productParentNode.appendChild(summerSeason)
+                break;
+                
+            case 'Autumn':
+                productParentNode.appendChild(autumnSeason)
+                break;
+                
+            case 'Winter':
+                productParentNode.appendChild(winterSeason)
+                break;    
+        }
+    }
+}
